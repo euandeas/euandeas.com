4f459c2896a96510fa6f3353ae471d5dafb5fa32
Euan <33450935+euandeas@users.noreply.github.com>
2024-06-05 22:54:17 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let data;
+
+    import ThemeSwitch from '$lib/components/theme-switch.svelte';
+</script>
+
+<svelte:head>
+	<title>WOAH</title>
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<ThemeSwitch />
+<!-- Posts -->
+<section>
+	<ul class="posts">
+		{#each data.posts as post}
+			<li class="post">
+				<a href="blog/{post.slug}" class="title">{post.title}</a>
+				<p class="date">{post.date}</p>
+				<p class="description">{post.description}</p>
+			</li>
+		{/each}
+	</ul>
+</section>
