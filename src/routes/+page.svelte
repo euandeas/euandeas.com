229c44542dbcbcd6a7250677e68538174e0ec1fe
Euan <33450935+euandeas@users.noreply.github.com>
2024-06-16 17:07:05 +0100

    feat: homepage outline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
-	export let data;
-
-    import ThemeSwitch from '$lib/components/theme-switch.svelte';
+	import ThemeSwitch from '$lib/components/theme-switch.svelte';
 </script>
 
 <svelte:head>
 	<title>WOAH</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<ThemeSwitch />
-<!-- Posts -->
+<div class="relative h-screen flex flex-col items-center justify-center">
+	<div class="absolute top-0 right-0 p-4">
+		<ThemeSwitch />
+	</div>
+  
+	<!-- The container for the two columns -->
+	<div class="w-full max-w-[960px] flex flex-col md:flex-row justify-center mx-10">
+		<div class="flex-1 p-4 mx-2 mb-2 md:mb-0 text-center md:text-left">
+			<h1 class="text-6xl font-bold">Euan Deas</h1>
+      		<h2 class="text-2xl text-gray-600 mt-2">Student, Back-end Software Dev & Endurance Athlete</h2>
+		  </div>
+		<div class="flex-1 p-4 mx-2 mb-2 md:mb-0 flex items-center justify-center">
+			<ul class="space-y-4 text-center md:text-left">
+				<li><a href="#about" class="text-2xl hover:underline">About</a></li>
+				<li><a href="#projects" class="text-2xl hover:underline">Projects</a></li>
+				<li><a href="#blog" class="text-2xl hover:underline">Blog</a></li>
+			  </ul>
+		</div>
+	  </div>
+  </div>
+<!-- Posts 
 <section>
 	<ul class="posts">
 		{#each data.posts as post}
@@ -23,4 +37,4 @@
 			</li>
 		{/each}
 	</ul>
-</section>
+</section>-->
