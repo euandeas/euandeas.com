edcc046e6ae085422624c38db140335e63bc690e
Euan <33450935+euandeas@users.noreply.github.com>
2024-06-18 18:45:54 +0100

    fix: site now builds while there are 0 blog posts

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let data
+</script>
+
+<section>
+	<ul class="posts">
+		{#each data.posts as post}
+			<li class="post">
+				<a href="blog/{post.slug}" class="title">{post.title}</a>
+				<p class="date">{post.date}</p>
+				<p class="description">{post.description}</p>
+			</li>
+		{/each}
+	</ul>
+</section>
