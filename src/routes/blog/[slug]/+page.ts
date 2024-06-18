7cdc035549ac46c2ea18116280c9ae02b196b559
Euan <33450935+euandeas@users.noreply.github.com>
2024-06-18 17:11:27 +0100

    feat: testing changes

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,14 +1,14 @@
 import { error } from '@sveltejs/kit';
 
-export async function load({ params }) {
+export async function load({ params: { slug } }) {
 	try {
-		const post = await import(`../../../posts/${params.slug}.md`);
+		const post = await import(`../../../posts/${slug}/${slug}.md`);
 
 		return {
 			content: post.default,
 			meta: post.metadata
 		};
 	} catch (e) {
-		error(404, `Could not find ${params.slug}`);
+		error(404, `Could not find ${slug}`);
 	}
 }
