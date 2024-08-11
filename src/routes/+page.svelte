06f161efeb651e8d8ed2eee164d1d9b004d15746
Euan <33450935+euandeas@users.noreply.github.com>
2024-08-11 12:31:32 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,13 +15,28 @@
 	<title>Euan Deas</title>
 </svelte:head>
 
-<div>
-	<Header {isSticky} />
+<div class="fixed top-0 right-0 p-3 z-20">
+	<ThemeSwitch />
+</div>
 
-	<div class="pt-16">
-		<!-- Adjust padding-top to accommodate header height -->
-		<div class="h-screen bg-secondary p-16">1</div>
-		<div class="h-screen bg-secondary p-16">2</div>
-		<div class="h-screen bg-secondary p-16">3</div>
+<div
+	class="bg-background transition-all duration-700 fixed w-full text-center flex flex-col items-center justify-center h-screen z-0"
+>
+	<div
+		class="transition-all duration-700 w-full max-w-[1400px] flex md:flex-row flex-col justify-center mx-10 min-h-full"
+	>
+		<div class="flex-1 px-10 md:text-left flex items-center justify-start">
+			<div class="flex-col">
+				<h1 class="transition-all duration-700 font-bold text-6xl">Euan Deas</h1>
+				<h2 class="transition-all duration-700 text-gray-600 text-2xl mt-2">
+					Student, Back-end Software Dev & Endurance Athlete
+				</h2>
+			</div>
+		</div>
+		<div class="flex-1 px-10 mb-0 flex flex-col md:justify-center md:pt-0 pt-10">
+			<p class="text-3xl">
+				New Page Coming Soon!
+			</p>
+		</div>
 	</div>
 </div>
